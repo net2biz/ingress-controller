3b9cb06cce0fa2371855b135e1b72e3cb1b62b27
Sanjay Patel <28770532+net2biz@users.noreply.github.com>
2021-09-25 19:42:30 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,8 +15,24 @@ kubectl get pods
 kubectl expose pod nginx  --port=80 --type=NodePort
 kubectl get service
 
+# updating the host file with ip address of the minikube
+echo "$(minikube ip) example.com" | sudo tee -a /etc/hosts
+
+# wait for 3 seconds for background processes to sync up
+sleep 3
+
+# creating self signed certs
 ./self_sign_cert.sh
 
 # creating ingress 
 kubectl create -f ingress.yaml
+
+# testing the SSL certificate. Below command should return the nginx webpage 
+curl -k https://example.com
+
+# testing the SSL certificate. Below command should return the nginx webpage 
+curl --cacert tls.crt https://example.com
+
+
+
 
